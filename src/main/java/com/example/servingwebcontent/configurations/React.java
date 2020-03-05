45ad5d5ad98930aabdabfa4788b7a8784169f015
GeorgeKologermanskiy <georgekolog@gmail.com>
2020-03-05 18:34:00 +0300

    add conf

---
+++ src/main/java/com/example/servingwebcontent/configurations/React.java
@@ -0,0 +1,29 @@
+package com.example.servingwebcontent.configurations;
+
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import java.io.IOException;
+
+
+@Component
+public class React {
+
+    @Value(value = "classpath:dist/bundle.js")
+    private Resource bundleJsFile;
+
+    private NashornScriptEngine getNashornScriptEngine() throws ScriptException, IOException {
+
+        NashornScriptEngine nashornScriptEngine = (NashornScriptEngine) new ScriptEngineManager().getEngineByName ("nashorn");
+
+        nashornScriptEngine.eval ("load ('" + bundleJsFile.getFile().getCanonicalPath() + "')");
+
+        return nashornScriptEngine;
+    }
+}
